70cb508a3f72abe7fe3f2667eaf6d27307578b7f
safonovli <safonovli@wbs-group.ru>
2015-10-17 22:34:42 +0300

    Create text.php

---
+++ views/fbuilder/default/text.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.');?>
+<div class="form-group">
+<? echo Form::label($name, isset($label)?$label:$name,$label_params) ?>
+<div class="col-lg-9">
+
+<?echo Form::input($name, $value,$params);?>
+
+<?if(isset($errors)):?>
+<span class="error"><? echo $errors ?> </span>
+<? endif; ?>
+</div>
+</div>
