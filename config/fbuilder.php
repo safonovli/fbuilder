f867fe0e62b2379d3f09638c83fbbc6c7285f4a6
safonovli <safonovli@wbs-group.ru>
2015-10-17 22:28:07 +0300

    Create fbuilder.php

---
+++ config/fbuilder.php
@@ -0,0 +1,18 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+return array(
+	'default'=>array(
+		'view'=>'fbuilder/mainterance',
+		'theme'=>'bootstrap3',
+		'params_default'=>array(
+			'button'=>array('class'=>'btn btn-default'),
+			'text'=>array('class'=>'form-control'),
+			'email'=>array('class'=>'form-control'),
+			'password'=>array('class'=>'form-control'),
+			'disabled'=>array('class'=>'form-control'),
+			'readonly'=>array('class'=>'form-control'),
+			'selector'=>array('class'=>'selectpicker'),
+			'label'=>array('class'=>'control-label col-lg-2'),
+			'textarea'=>array('style'=>'width:100%','class'=>'form-control'),
+		),
+	),
+);
